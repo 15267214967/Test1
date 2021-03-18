d09fcdcf3e2cd8eb8443119ec95eb3b506895087
williamliangbo_v <williamliangbo_v@didiglobal.com>
2021-03-18 18:11:21 +0800

    测试

---
+++ src/main/java/A.java
@@ -0,0 +1,11 @@
+/**
+ * @Classname A
+ * @Description TODO
+ * @Date 2021/3/18 6:10 下午
+ * @Created by liangbo
+ */
+public class A {
+    public static void main(String[] args) {
+        System.out.println("1111");
+    }
+}
